32a424aa7091dc9501a823948729157b1d3cc14c
luckylyk <lionel.brouyere@gmail.com>
2018-08-22 13:31:13 +0200

    simple generator window added

---
+++ applications/montunomakina/generators.py
@@ -0,0 +1,40 @@
+from PyQt5 import QtWidgets, QtCore
+
+
+class SimpleGenerator(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent, QtCore.Qt.Window)
+
+        self._patterneditors = []
+        self._chordeditors = []
+
+        self._patterneditor_combo = QtWidgets.QComboBox()
+        self._patternfilepath_lineedit = QtWidgets.QLineEdit()
+        self._patternfilepath_lineedit.setPlaceHolderText('use pattern file')
+
+        self._chordseditor_combo = QtWidgets.QComboBox()
+        self._chordspath_lineedit = QtWidgets.QLineEdit()
+        self._chordspath_lineedit.setPlaceHolderText('use chord file')
+        
+        self._data_layout = QtWidgets.QFormLayout()
+        self._data_layout.addRow('select pattern', self._patterneditor_combo)
+        self._data_layout.addRow(self._patternfilepath_lineedit)
+        self._data_layout.addRow('select chord', self._chordseditor_combo)
+        self._data_layout.addRow(self._chordspath_lineedit)
+
+        self._generator = QtWidget.QPushButton('Generate')
+        self._convertor = QtWidget.QPushButton('Convert')
+
+        self._buttons_layout = QtWidget.QHBoxLayout()
+        self._buttons_layout.addWidget(self._generator)
+        self._buttons_layout.addWidget(self._convertor)
+
+        self._layout = QtWidgets.QVBoxLayout(self)
+        self._layout.addWidget(self._data_layout)
+        self._layout.addWidget(self._buttons_layout)
+
+    def set_patterneditors(self, patterneditors):
+        self._patterneditors = patterneditors
+
+    def set_chordeditors(self, chordeditors):
+        self._chordeditors = chordeditors
